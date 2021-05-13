abac39b13d18cbcd640c7bbeee623f68aa7d0773
Chan <l035831@naver.com>
2021-05-13 17:19:33 +0900

    JOIN

--- HR5.sql
+++ HR5.sql
@@ -1,35 +1,16 @@
---  CASE 함수
---  보너스를 지급하기로 했다
---  AD관련 직원에게는 20%, SA 관련 직원에게는 10%, IT관련 직원에게는 8%
---  나머지는 5%
-
-select first_name, job_id, salary, SUBSTR(job_id, 1, 2),
-    case substr(job_id, 1, 2) when 'AD' then salary * 0.2
-                                when 'SA' then salary * 0.1
-                                when 'IT' then salary * 0.08
-                                else salary * 0.05
-    end as bonus
-from employees;
-
---  Decode
-select first_name, salary, job_id, substr(job_id, 1, 2),
-    decode(substr(job_id, 1, 2),
-        'AD', salary * 0.2,
-        'SA', salary * 0.1,
-        'IT', salary * 0.08,
-        salary * 0.05) as bonus
-from employees;
-
---  푸랙티스
---  department_id <= 30 -> A-group
---  department_id <= 50 -> B-group
---  department_id <= 100 -> C-group
---  나머지 : 
-select first_name, department_id,
-    case when department_id <= 30 then 'A-Group'
-        when department_id <= 50 then 'B-Group'
-        when department_id <= 100 then 'C-Group'
-        else 'Remainder'
-    end as team
+--  문제 1
+select first_name, last_name, salary, phone_number, hire_date
 from employees
-order by team;
+order by hire_date;
+--  문제2
+select job_id, salary
+from employees
+order by salary DESC;
+--  문제 3
+select first_name, manager_id, nvl(commission_pct, 0), salary
+from employees
+WHERE salary > 3000;
+
+--  문제 4
+select 
+from employees;
